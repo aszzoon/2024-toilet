ef57eb5121716b4d35e3c0e30ad946f9bee3808a
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-04-08 14:39:25 +0900

    [BE]properties

---
+++ backend/src/main/java/com/toilet/backend/member/domain/Member.java
@@ -0,0 +1,18 @@
+package com.toilet.backend.member.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+}
